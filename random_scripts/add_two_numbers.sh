33e3d9d75b7162f7ba38ae4a0a65246a40ddd14a
U-DESKTOP-UA5PD4J\annec <annecalija@gmail.com>
2019-06-24 21:16:41 +0800

    add random script - adds 2 numbers from env vars

---
+++ random_scripts/add_two_numbers.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+echo "this script is adding 2 numbers - fetching from environment variables."
+
+SUM=$(($NUM1 + $NUM2))
+
+echo "the sum of the numbers NUM1 and NUM2 is: "
+echo $SUM
